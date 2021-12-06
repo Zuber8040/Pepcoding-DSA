d623fe18c86d3602c14c5b84cbac172abe514890
Zuber8040 <zuberustad456@gmail.com>
2021-12-06 10:09:35 +0530

    matrix added

--- Basic Programming/2D Array/Matrix_Multiplication.cpp
+++ Basic Programming/2D Array/Matrix_Multiplication.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
+
 int main(){
 int r1,c1;
+
 cin>>r1>>c1;
+
 int arr1[r1][c1];
+
 for(int i=0;i<r1;i++){
     for(int j=0;j<c1;j++){
         cin>>arr1[i][j];
