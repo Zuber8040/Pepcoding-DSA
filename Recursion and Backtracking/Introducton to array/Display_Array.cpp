e669ffbfd1e87dcfd61f76bbc59c86b996dcdba9
Zuber8040 <zuberustad456@gmail.com>
2021-12-15 12:53:07 +0530

    started array

---
+++ Recursion and Backtracking/Introducton to array/Display_Array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void display(int arr[], int idx, int n){
+    // write your code here
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    display(arr, 0, n);
+}
