a232c520590cb16d68b57966898947a8714a9466
Zuber8040 <zuberustad456@gmail.com>
2021-12-19 18:11:44 +0530

    done recrusion with array

---
+++ Recursion and Backtracking/Introducton to array/First_Index.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+
+
+int firstIndex(int arr[], int idx, int x, int n){
+    // write your code here
+    if(idx==n){
+        return -1;
+    }
+    int fisa = firstIndex(arr,idx+1,x,n);
+    if(arr[idx]==x){
+        return idx;
+    }
+    else{
+        return fisa;
+    }
+}
+
+int main()
+
+{
+    int n;
+    cin >> n;
+    int d;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> d;
+    int p = firstIndex(arr, 0, d, n);
+    cout << p << endl;
+
+}
--- Recursion and Backtracking/Introducton to array/Max_Of_An_Array.cpp
+++ Recursion and Backtracking/Introducton to array/Max_Of_An_Array.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int max(int arr[], int idx, int n){
-    
+int maxOfArray(int arr[], int idx, int n){
 
+if(idx==n-1){
+    return arr[idx];
+}
+
+
+int maxe = maxOfArray(arr,idx+1,n);
+if(maxe>arr[idx]){
+    return maxe;
+}
+else{
+    return arr[idx];
+}
 
 
 }
@@ -14,6 +25,6 @@ int main(){
     int arr[n];
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-    int p = max(arr, 0, n);
+    int p = maxOfArray(arr, 0, n);
     cout << p << endl;
 }
