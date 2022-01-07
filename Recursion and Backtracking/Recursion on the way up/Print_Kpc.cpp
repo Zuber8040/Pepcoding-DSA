3d810f1023566a44178dd44013e379b9e98b6a0a
Zuber8040 <zuberustad456@gmail.com>
2022-01-07 12:39:40 +0530

    done for the day

---
+++ Recursion and Backtracking/Recursion on the way up/Print_Kpc.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+string codes[] = {".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+void printKPC(string ques, string asf){
+
+    if(ques.length()==0){
+        cout<<asf<<"\n";
+        return ;
+    }
+   char ch = ques[0];
+   string roq = ques.substr(1); //rest of questions
+  int idx = ch-'0';
+
+for(int i=0;i<codes[idx].size();i++){
+        char ch2 = codes[idx].at(i);       
+        printKPC(roq,asf+ch2);
+    }
+
+    
+}
+
+int main(){
+    string str;
+    cin >> str;
+    printKPC(str, "");
+}
--- Recursion and Backtracking/Recursion on the way up/Print_Subsequence.cpp
+++ Recursion and Backtracking/Recursion on the way up/Print_Subsequence.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include<string>
 using namespace std;
 
 void printSS(string ques, string ans){
     // write your code here
-        
+    if(ques.length()==0){
+        cout<<ans<<"\n";
+        return ;
+    }
+    char ch = ques[0];
+    string roq = ques.substr(1);
+    printSS(roq,ans+ch);
+    printSS(roq,ans+"");
 }
 
 int main(){
