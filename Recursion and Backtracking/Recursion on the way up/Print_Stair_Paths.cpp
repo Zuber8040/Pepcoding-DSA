2c771840489a20ada1da96d4f23d185ddc507b00
Zuber8040 <zuberustad456@gmail.com>
2022-01-09 13:24:02 +0530

    done stirs_path

--- Recursion and Backtracking/Recursion on the way up/Print_Stair_Paths.cpp
+++ Recursion and Backtracking/Recursion on the way up/Print_Stair_Paths.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include<vector>
+#include<string>
 using namespace std;
 
 void printStairPaths(int n, string psf){
     // write your code here
-    
+    // int count = 0;
+        if(n<0){
+            return ;
+        }
+        if(n==0 ){
+            cout<<psf<<"\n";
+            return ;
+        }
+        printStairPaths(n-1,psf+"1");
+        printStairPaths(n-2,psf+"2");
+        printStairPaths(n-3,psf+"3");
 }
 
-int main(){
 
+
+int main(){
     int n;
     cin >> n;
     printStairPaths(n, "");
