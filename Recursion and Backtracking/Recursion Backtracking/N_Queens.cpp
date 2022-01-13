c7a160f780da692c2114df96dd0c9dfec5471877
Zuber8040 <zuberustad456@gmail.com>
2022-01-13 12:33:50 +0530

    done

---
+++ Recursion and Backtracking/Recursion Backtracking/N_Queens.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printNQueens(vector<vector<int>> chess,string qsf,int row){
+  //write your code here
+    
+}
+
+int main(){
+  int n;
+  cin >> n;
+  vector<vector<int>> chess(n , vector<int> (n));
+    
+  printNQueens(chess,"",0);
+    
+}
